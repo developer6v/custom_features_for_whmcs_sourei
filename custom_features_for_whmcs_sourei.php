0892396827272a73248b80be863eb3852feceb9e
Otávio de Oliveira <otaviodeveloper6v@gmail.com>
2025-09-27 20:55:19 -0300

    fist commit

---
+++ custom_features_for_whmcs_sourei.php
@@ -0,0 +1,32 @@
+<?php
+
+include_once __DIR__ . '/hooks.php';
+include_once __DIR__ . '/src/Views/config.php';
+include_once __DIR__ . '/src/Config/assets.php';
+
+function custom_features_for_whmcs_sourei_config() { 
+    return array(
+        'name' => 'Custom Features For WHMCS',
+        'description' => 'Módulo responsável por customizações no WHMCS.',
+        'version' => '1.0',
+        'author' => 'Sourei',
+        'fields' => array()
+    );
+}
+
+function custom_features_for_whmcs_sourei_activate() {
+    return array('status' => 'success', 'description' => 'Módulo ativado com sucesso!');
+}
+
+function custom_features_for_whmcs_sourei_deactivate() {
+    return array('status' => 'success', 'description' => 'Módulo desativado com sucesso!');
+}
+
+function custom_features_for_whmcs_sourei_output() {
+    echo assets();
+    echo config();
+}
+
+
+
+?>
