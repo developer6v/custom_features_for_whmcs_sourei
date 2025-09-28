dfcdbac7364c26e38d31292d2c83130c7b0a5f3a
Otávio de Oliveira <otaviodeveloper6v@gmail.com>
2025-09-27 21:17:25 -0300

    estrutura

--- src/Config/Assets/login.php
+++ src/Config/Assets/login.php
@@ -2,7 +2,7 @@
 
 
 function assets_login() {
-    $base_url = 'https://' . $_SERVER['HTTP_HOST'] . '/modules/addons/custom_features_for_whmcs/public';
+    $base_url = 'https://' . $_SERVER['HTTP_HOST'] . '/modules/addons/custom_features_for_whmcs_sourei/public';
     $version = time(); 
     $assets = '<link rel="stylesheet" type="text/css" href="' . $base_url . '/css/assets.css?v=' . $version . '">';
     $assets .= '<script type="text/javascript" src="' . $base_url . '/js/assets.js?v=' . $version . '"></script>';
