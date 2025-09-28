8a7d909a8c058b360ea32ec072a6256c30e11f68
Otávio de Oliveira <otaviodeveloper6v@gmail.com>
2025-09-27 21:01:29 -0300

    solving

--- hooks.php
+++ hooks.php
@@ -1,4 +1,19 @@
-<?php 
+<?php
 
+use WHMCS\View\Template\Smarty;
 
+// Hook que roda no footer do Client Area
+add_hook('ClientAreaFooterOutput', 1, function($vars) {
+    // Detecta a página atual
+    $currentFile = basename($_SERVER['PHP_SELF']);
 
+    if ($currentFile === 'logincustom.php') {
+        return <<<HTML
+<script>
+    alert('⚠️ Aviso: Esta é a página de login customizada!');
+</script>
+HTML;
+    }
+
+    return '';
+});
