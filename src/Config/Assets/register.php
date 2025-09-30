c408d207be7862b5f793d616a6d80ef110c676ae
Otávio de Oliveira <otaviodeveloper6v@gmail.com>
2025-09-30 14:49:30 -0300

    estilo

--- src/Config/Assets/register.php
+++ src/Config/Assets/register.php
@@ -6,13 +6,13 @@ function assets_register() {
 
     
     // Registra os assets do intl-tel-input
-    $assets .= '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/css/intlTelInput.css">';
+    $assets = '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/css/intlTelInput.css">';
     $assets .= '<script src="https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/intlTelInput.min.js"></script>';
     $assets .= '<script src="https://cdnjs.cloudflare.com/ajax/libs/inputmask/5.0.7/inputmask.min.js"></script>';
 
 
     // Registra os assets personalizados
-    $assets = '<link rel="stylesheet" type="text/css" href="' . $base_url . '/css/register.css?v=' . $version . '">';
+    $assets .= '<link rel="stylesheet" type="text/css" href="' . $base_url . '/css/register.css?v=' . $version . '">';
     $assets .= '<script type="text/javascript" src="' . $base_url . '/js/register.js?v=' . $version . '"></script>';
 
     return $assets;
