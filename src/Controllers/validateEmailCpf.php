279498d8fb9844eb674ffb5d666d5127f9fe5bf0
Otávio de Oliveira <otaviodeveloper6v@gmail.com>
2025-10-09 08:43:56 -0300

    register custom

--- src/Controllers/validateEmailCpf.php
+++ src/Controllers/validateEmailCpf.php
@@ -1,13 +1,14 @@
 <?php
 include_once('../../../../../init.php'); 
 use WHMCS\Database\Capsule;
+header('Content-Type: application/json; charset=utf-8');
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     $data = json_decode(file_get_contents("php://input"), true);
 
-    $cpf = isset($data['cpf']) ? (int) $data['cpf'] : null;
-    $email = isset($data['email']) ? (int) $data['email'] : null;
+    $cpf = isset($data['cpf']) ? (string) $data['cpf'] : null;
+    $email = isset($data['email']) ? (string) $data['email'] : null;
     $cpfDigits = preg_replace('/\D+/', '', (string)$cpf);
 
     if ($cpf === null || $email === null) {
@@ -21,9 +22,19 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             ->whereRaw("REGEXP_REPLACE(value, '[^0-9]', '') = ?", [$cpfDigits])
             ->first();
 
+        $resultsClients = localAPI('GetClients', array('search'=>$email));
+        $qtdDeClientes = $resultsClients["totalresults"] > 0;
+        $cpfIsset = isset($cpfBanco);
 
-            // Retorna sucesso
-            echo json_encode(['status' => 'success', 'message' => 'cpf: ' . $cpfBanco]);
+
+        $cpfDbDigits = preg_replace('/\D+/', '', (string)$cpfBanco->value);
+        echo json_encode([
+            'status'  => 'success',
+            'cpf'     => ($cpfIsset),
+            'email'=> $qtdDeClientes,
+        ], JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES);
+    
+        exit;
     } catch (Exception $e) {
         // Em caso de erro, retorna a mensagem de erro
         echo json_encode(['status' => 'error', 'message' => 'Erro ao atualizar configurações: ' . $e->getMessage()]);
